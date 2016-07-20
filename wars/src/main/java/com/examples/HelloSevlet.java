253e932ae2e99ef63984b9a9820aa3deb2f333d9
Ghada <ghada.drissi@esprit.tn>
2016-07-20 16:17:33 +0100

    Signed-off-by: Ghada <ghada.drissi@esprit.tn>

--- wars/src/main/java/com/examples/HelloSevlet.java
+++ wars/src/main/java/com/examples/HelloSevlet.java
@@ -20,7 +20,7 @@ public class HelloSevlet extends HttpServlet {
     public HelloSevlet() {
         super();
         // TODO Auto-generated constructor stub
-    }
+    }//
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
